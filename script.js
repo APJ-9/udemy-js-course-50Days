7a5e268e52053e0e5fbb3c0160bbf09058f78cfc
APJ-9 <akhil.jose@vonnue.com>
2022-10-05 12:21:20 +0530

    modified the script tag

--- script.js
+++ script.js
@@ -10,16 +10,13 @@ function jsonconnection() {
     xhr.onload = function () {
         if (this.status == 200) {
             const projects = JSON.parse(this.responseText)
-            // console.log(projects)
-            for (const key in projects) {
-                // console.log(typeof (projects[key]), projects[key])
-                const allProjects = projects[key]
-                let i = 1
-                allProjects.forEach(project => {
-                    // console.log(i, project)
-                    const card = document.createElement('div')
-                    card.classList.add('card')
-                    card.innerHTML = `
+            const allProjects = projects.projects
+            let i = 1
+            allProjects.forEach(project => {
+                // console.log(i, project)
+                const card = document.createElement('div')
+                card.classList.add('card')
+                card.innerHTML = `
                             <div class="project-number">
                                 <span>${i}</span>
                             </div>
@@ -28,10 +25,10 @@ function jsonconnection() {
                                 <a href="${project.link}"> <button>View Project</button> </a>
                             </div>
                         `
-                    mainSection.appendChild(card)
-                    i++
-                });
-            }
+                mainSection.appendChild(card)
+                i++
+            });
+
 
         }
     }
